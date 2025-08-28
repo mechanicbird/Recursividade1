c6ea6a4e61ddb5daba4f912e2c0b237b4d050772
Nanda <flornanda07@gmail.com>
2025-08-27 21:59:55 -0300

    Add files via upload

---
+++ Filas01/IncomingCall.cs
@@ -0,0 +1,10 @@
+using System;
+public class IncomingCall
+{
+ public int Id { get; set; }
+ public int ClientId { get; set; }
+ public DateTime CallTime { get; set; }
+ public DateTime StartTime { get; set; }
+ public DateTime EndTime { get; set; }
+ public string Consultant { get; set; }
+}
